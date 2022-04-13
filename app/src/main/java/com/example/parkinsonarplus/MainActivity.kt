51e09e083f98497ecfbb99b47edbc3d4a39c25b7
pedroTirado <17729184+pedroTirado@users.noreply.github.com>
2022-04-13 17:28:12 -0400

    set up tremor button listener event in  HomeFragment.kt (prints "swag" to console)

--- app/src/main/java/com/example/parkinsonarplus/MainActivity.kt
+++ app/src/main/java/com/example/parkinsonarplus/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.parkinsonarplus
 
 import android.os.Bundle
+import android.widget.Button
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -32,4 +33,5 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
+
 }
--- app/src/main/java/com/example/parkinsonarplus/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/parkinsonarplus/ui/home/HomeFragment.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+//import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+//import com.example.parkinsonarplus.R
 import com.example.parkinsonarplus.databinding.FragmentHomeBinding
+
+//import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
@@ -32,8 +36,14 @@ class HomeFragment : Fragment() {
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+        binding.buttonTremor.setOnClickListener {
+            println("swag")
+        }
+
         return root
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
